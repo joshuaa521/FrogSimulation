2c39175cd3050e364a0d65bbbdc8172f7fe0950a
Game <joshuaa521@nycstudents.net>
2025-11-17 01:50:50 -0500

    Added test case

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
 public class Main {
-    FrogSimulation sim = new FrogSimulation(24,5);
+    public static void main(String[] args)
+    {
+        FrogSimulation sim = new FrogSimulation(24, 5);
+        System.out.println(sim.runSimulations(400));
+    }
 }
